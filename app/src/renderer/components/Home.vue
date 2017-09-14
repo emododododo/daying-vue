cd9624aba5d17cda33264ec537b1ccc51771b458
smackxu <smackxu@raventech.com>
2017-09-14 15:39:38 +0800

    update home page

--- app/src/renderer/components/Home.vue
+++ app/src/renderer/components/Home.vue
@@ -1,56 +1,139 @@
 <template>
-  <div>
-    <div class="nav">
-      <input type="text" name="" :value="url">
-    </div>
-    <webview
-      src="https://www.baidu.com/"
-      class="webview"
-    ></webview>
+  <div class="wrapper">
+    <ul class="images-wrapper">
+      <li v-for="item in source" @click="handleClick(item.url)">
+        <img :src="item.icon" alt="electron-vue">
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import CurrentPage from './LandingPageView/CurrentPage'
-  import Links from './LandingPageView/Links'
-  import Versions from './LandingPageView/Versions'
+  const source = [
+    {
+      icon: require('./assets/aqylogo.png'),
+      url: 'http://www.iqiyi.com/'
+    },
+    {
+      icon: require('./assets/youkulogo.png'),
+      url: 'http://www.youku.com/'
+    },
+    {
+      icon: require('./assets/letvlogo.png'),
+      url: 'http://www.le.com/'
+    },
+    {
+      icon: require('./assets/qqlogo.png'),
+      url: 'http://v.qq.com/'
+    },
+    {
+      icon: require('./assets/tudoulogo.png'),
+      url: 'http://www.tudou.com/'
+    },
+    {
+      icon: require('./assets/sohulogo.png'),
+      url: 'http://tv.sohu.com/'
+    },
+    {
+      icon: require('./assets/56logo.png'),
+      url: 'http://www.56.com/'
+    },
+    {
+      icon: require('./assets/ku6logo.png'),
+      url: 'http://www.ku6.com/'
+    },
+    {
+      icon: require('./assets/wasulogo.png'),
+      url: 'http://www.wasu.cn/'
+    },
+    {
+      icon: require('./assets/yinyuetailogo.png'),
+      url: 'http://www.yinyuetai.com/'
+    },
+    {
+      icon: require('./assets/hunantvlogo.png'),
+      url: 'http://www.mgtv.com/'
+    },
+    {
+      icon: require('./assets/sinalogo.png'),
+      url: 'http://video.sina.com.cn/'
+    },
+    {
+      icon: require('./assets/163logo.png'),
+      url: 'http://v.163.com/'
+    },
+    {
+      icon: require('./assets/6logo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/kugoulogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/baomihualogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/ifenglogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/kankannewslogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/mtimelogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/kuwologo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/1905logo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/tangdoulogo.png'),
+      url: '#'
+    },
+    {
+      icon: require('./assets/cntvlogo.png'),
+      url: '#'
+    }
+  ]
+
+  import { mapActions } from 'vuex'
   export default {
     data () {
       return {
-        url: 'https://www.baidu.com/'
+        source
       }
     },
-    components: {
-      CurrentPage,
-      Links,
-      Versions
+    methods: {
+      ...mapActions(['setUrl']),
+      handleClick (url) {
+        this.setUrl(url)
+        this.$router.push({
+          path: '/webview_view'
+        })
+      }
     },
     name: 'home'
   }
 </script>
 
-<style scoped>
-  img {
-    margin-top: -25px;
-    width: 450px;
-  }
-
-  .webview {
+<style scoped lang="scss">
+  .images-wrapper {
+    display: flex;
     width: 100vw;
-    height: calc(100vh - 30px);
-    position: absolute;
-    top: 30px;
-    left: 0;
-  }
-
-  .nav {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 30px;
-
-    input {
-      width: 100%;
+    flex-wrap: wrap;
+    img {
+      width: 110px;
+      padding: 16px 14px;
+      margin: 10px;
     }
   }
+
 </style>
